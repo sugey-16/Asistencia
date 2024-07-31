177fd468e5fb5376059096f31bc2a4497f8fc1de
mady1234567817 <mady1234567817@outlook.com>
2024-07-31 13:23:40 -0600

    proyecto de asistencia

---
+++ src/test/java/com/esfeasistencia/Asistencia/AsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esfeasistencia.Asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
